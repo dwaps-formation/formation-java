76a4b4967f1a12347b1951cc23098c5795187a32
Michael Cornillon <contact@dwaps.fr>
2017-10-18 14:25:36 +0200

    Affichage du mois du jour

--- src/PremiereClasse.java
+++ src/PremiereClasse.java
@@ -3,16 +3,34 @@ import java.util.Scanner;
 public class PremiereClasse {
 
 	public static void main(String[] args) {
-		String tab[] = { "Tu as ", " ans", " et demi" };
+		String datesBuilder[][] = {
+			{ "", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre" },
+			{ "", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche" }
+		};
+		
 		Scanner sc = new Scanner(System.in);
-		int age;
+		int nbOfDay = 0, month = 0;
 		
-		System.out.print("Quel age as-tu ? ");
-		age = sc.nextInt();
+		System.out.print("Choississez un nombre entre 1 et 365 pour obtenir le mois du jour correspondant : ");
+		nbOfDay = sc.nextInt();
 		
-		for (int i = 0; i < tab.length; i++) {
-			System.out.print(tab[i] + (i == 0 ? age : ""));
-		}
+		if (nbOfDay < 31*1) month = 1;
+		else if (nbOfDay <= (31*1 + 30*1-1)) month = 2;
+		else if (nbOfDay <= (31*2 + 30*1-1)) month = 3;
+		else if (nbOfDay <= (31*2 + 30*2-1)) month = 4;
+		else if (nbOfDay <= (31*3 + 30*2-1)) month = 5;
+		else if (nbOfDay <= (31*3 + 30*3-1)) month = 6;
+		else if (nbOfDay <= (31*4 + 30*3-1)) month = 7;
+		else if (nbOfDay <= (31*5 + 30*3-1)) month = 8;
+		else if (nbOfDay <= (31*5 + 30*4-1)) month = 9;
+		else if (nbOfDay <= (31*6 + 30*4-1)) month = 10;
+		else if (nbOfDay <= (31*6 + 30*5-1)) month = 11;
+		else if (nbOfDay <= (31*7 + 30*5-1)) month = 12;
+		
+		sc.nextLine();
+		System.out.println("\n" + datesBuilder[0][month]);
+		
+		sc.close();
 	}
 
 }
