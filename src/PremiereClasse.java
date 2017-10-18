0e00568839068d58b466b1526980a8f42b8b8b62
Michael Cornillon <contact@dwaps.fr>
2017-10-18 14:21:23 +0200

    Affichage phrase via condition simple

--- src/PremiereClasse.java
+++ src/PremiereClasse.java
@@ -1,10 +1,38 @@
+import java.io.PrintStream;
+
 public class PremiereClasse {
 
 	public static void main(String[] args) {
-		String tab[] = { "Tu as ", " ans" };
-		int age = 18;
+		PrintStream out = System.out;
+		String tab[] = { "Tu as ", " ans", " et demi" };
+		int age = 56;
 		
-		System.out.println(tab[0] + age + tab[1]);
+		// Proposition de possibilité 1
+		for (int i = 0; i < tab.length; i++) {
+			if (i == 0) out.print(tab[i] + age);
+			else out.print(tab[i]);
+		}
+		
+		out.println();
+		
+		// Proposition de possibilité 2
+		for (String str : tab) {
+			if (str.equals(tab[tab.length-2])) {
+				out.print(age + str);
+			} else {
+				out.print(str);
+			}
+		}
+		
+		out.println();
+		
+		// Proposition de possibilité 3
+		for (int i = 0; i < tab.length; i++) {
+			if (i % 2 != 0) {
+				out.print(age);
+			}
+			out.print(tab[i]);
+		}
 	}
 
 }
